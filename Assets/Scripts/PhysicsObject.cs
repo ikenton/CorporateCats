c58304b2dc421e7dd3e23896461f0cf79051a03d
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-03-25 16:52:19 -0700

    Changed player to dynamic rigidbody

--- Assets/Scripts/PhysicsObject.cs
+++ Assets/Scripts/PhysicsObject.cs
@@ -20,29 +20,29 @@ public class PhysicsObject : MonoBehaviour
         Vector2 movement = velocity * Time.fixedDeltaTime;
         velocity.x = desiredx;
         Movement(new Vector2(movement.x, 0), true);
-        Movement(new Vector2(0, movement.y), false);
+        //Movement(new Vector2(0, movement.y), false);
     }
 
     void Movement (Vector2 move, bool movex)
     {
-        if (move.magnitude < 0.00001f) return;
-        RaycastHit2D[] results = new RaycastHit2D[16];
-        int cnt = GetComponent<Rigidbody2D>().Cast(move, results, move.magnitude + 0.01f);
-        for (int i = 0; i < cnt; i++)
-        {
-            if (Mathf.Abs(results[i].normal.x) > 0.5 && movex)
-            {
-                move.x = 0;
-                velocity.x = 0;
-                CollideWithHorizontal(results[i].collider);
-            }
-            if (Mathf.Abs(results[i].normal.y) > 0.5 && !movex)
-            {
-                move.y = 0;
-                velocity.y = 0;
-                CollideWithVertical(results[i].collider);
-            }
-        }
+        //if (move.magnitude < 0.00001f) return;
+        //RaycastHit2D[] results = new RaycastHit2D[16];
+        //int cnt = GetComponent<Rigidbody2D>().Cast(move, results, move.magnitude + 0.01f);
+        //for (int i = 0; i < cnt; i++)
+        //{
+        //    if (Mathf.Abs(results[i].normal.x) > 0.5 && movex)
+        //    {
+        //        move.x = 0;
+        //        velocity.x = 0;
+        //        CollideWithHorizontal(results[i].collider);
+        //    }
+        //    if (Mathf.Abs(results[i].normal.y) > 0.5 && !movex)
+        //    {
+        //        move.y = 0;
+        //        velocity.y = 0;
+        //        CollideWithVertical(results[i].collider);
+        //    }
+        //}
 
         transform.position += (Vector3)move;
     }
