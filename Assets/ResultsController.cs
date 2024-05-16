f1a6147a127f26e7e33ad3d608d9099efda9d509
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-05-16 11:20:48 -0700

    cutscene work

--- Assets/ResultsController.cs
+++ Assets/ResultsController.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class ResultsController : MonoBehaviour
 {
@@ -12,6 +14,9 @@ public class ResultsController : MonoBehaviour
     public TMP_Text overallText;
 
     public Button resultsButton;
+    public VideoPlayer fail;
+    public VideoPlayer pass;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +24,16 @@ public class ResultsController : MonoBehaviour
         string pouncingGrade = (InterviewManager.Instance.pouncingGrade*100).ToString("F2");
         string climbingGrade = (InterviewManager.Instance.climbingGrade*100).ToString("F2");
         string bakingGrade = (InterviewManager.Instance.bakingGrade*100).ToString("F2");
-        string overall = ((InterviewManager.Instance.pouncingGrade + InterviewManager.Instance.climbingGrade + InterviewManager.Instance.bakingGrade) / 3).ToString("F2");
+        string overall = ((InterviewManager.Instance.pouncingGrade + InterviewManager.Instance.climbingGrade) / 2).ToString("F2");
 
         pouncingText.text = "Pouncing: " + pouncingGrade + "%";
         climbingText.text = "Climbing: " + climbingGrade + "%";
         bakingText.text = "Baking: " + bakingGrade + "%";
         overallText.text = "Overall: " + overall + "%";
+
+        float failTime = (float)fail.length;
+        float passTime = (float)pass.length;
+
 
         resultsButton.onClick.AddListener(() =>
         {
@@ -35,11 +44,26 @@ public class ResultsController : MonoBehaviour
     void PlayResultsCutscene()
     {
         Debug.Log("Playing results cutscene");
+        if ((InterviewManager.Instance.pouncingGrade + InterviewManager.Instance.climbingGrade) / 2 > 0.7f)
+        {
+            pass.Play();
+            Invoke("BackToMain", (float)pass.length);
+        }
+        else
+        {
+            fail.Play();
+            Invoke("BackToMain", (float)fail.length);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void BackToMain()
+    {
+        SceneManager.LoadScene("Overworld");
     }
 }
