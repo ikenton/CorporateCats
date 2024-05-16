5b5bd55dddab1b25b3f7d3f3790c13f5ab48fc83
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-05-15 22:29:10 -0700

    interview manager prep

--- Assets/Scripts/InterviewManager.cs
+++ Assets/Scripts/InterviewManager.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class InterviewManager : MonoBehaviour
 {
     public static InterviewManager Instance;
     public bool isAutoplay;
+    public string[] levels = new string[]
+    {
+        "QuickTimeEvent",
+        "ClimbingEvent",
+        "BakingEvent",
+    };
 
     private void Awake()
     {
@@ -30,4 +38,22 @@ public class InterviewManager : MonoBehaviour
     {
         
     }
+
+    public void NextStage()
+    {
+        for (int i = 0; i < levels.Length; i++)
+        {
+            if (SceneManager.GetActiveScene().name == levels[i])
+            {
+                if (i == levels.Length - 1)
+                {
+                    SceneManager.LoadScene("Overworld");    // temp, move to final scene later
+                }
+                else
+                {
+                    SceneManager.LoadScene(levels[i + 1]);
+                }
+            }
+        }
+    }   
 }
